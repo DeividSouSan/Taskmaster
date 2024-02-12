81248a59eb0688aea992294a2379edb7f367d070
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-12 14:37:58 -0300

    feat: register form using wtforms

---
+++ src/forms/register.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Length
+
+
+class RegisterForm(FlaskForm):
+    username: str = StringField("Nome de Usuário: ", validators=[DataRequired(),  Length(max=20)])
+    password: str = PasswordField("Senha: ", validators=[DataRequired(), Length(max=100)])
+    fullname: str = StringField("Nome Completo: ", validators=[DataRequired(), Length(max=50)])
+    email: str = EmailField("E-mail: ", validators=[DataRequired(), Length(max=50)])
