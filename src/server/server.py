ae1a20e5b5b25815493a6b8b5e9316e6ebb9823f
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-12 14:39:19 -0300

    add: wtforms configurantion && engine

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,7 @@
-from src.routes import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from flask_wtf.csrf import CSRFProtect
 
 from dotenv import load_dotenv
 import os
@@ -8,7 +9,8 @@ import os
 load_dotenv()  # Carrega as variáveis de ambiente no .eInv
 
 app = Flask("__name__")
-app.register_blueprint(routes.bp)
+csrf = CSRFProtect(app)
+
 
 # Capturando as variáveis de ambiente configuradas
 USER = os.environ.get("USER")
@@ -18,10 +20,14 @@ DATABASE = os.environ.get("DATABASE")
 # Configurando a URI do banco de dados com as credenciais fornecidas
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '12345'
+app.config['WTF_CSRF_SECRET_KEY'] = '12345'
 
 # Se conectando ao banco de dados
 db = SQLAlchemy()
 db.init_app(app)
+engine = create_engine(
+    f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}')
 
 with app.app_context():
     from src.models.task import Task
@@ -30,3 +36,6 @@ with app.app_context():
     # Cria as tabelas
     db.create_all()
     print("As tabelas 'user' e 'task' foram criadas com sucesso.")
+
+from src.routes import routes
+app.register_blueprint(routes.bp)
