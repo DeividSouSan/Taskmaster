1414e5c80a026cad3b66127824c1132be5df18e4
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-12 14:41:32 -0300

    feat: implemented register user use case

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 from flask import Blueprint, request, jsonify, render_template
 from sqlalchemy import text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from src.forms.register import RegisterForm
+from src.server.server import engine
+
+from src.models.user import User
+from src.use_case.user.register_user_use_case import RegisterUserUseCase
 
 
 bp = Blueprint("tasks", __name__, template_folder="../templates")
 
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
 
-@bp.route("/", methods=["GET"])
-def index():
-    print('oi')
-    return render_template("index.html")
+    if request.method == "POST":
+        if form.validate_on_submit():
+            useCase = RegisterUserUseCase(form)
+            useCase.execute()
+
+    return render_template("sign_up.html", form=form)
