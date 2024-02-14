7aec35e5af46f386a937a20f4982f74892fb83df
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-14 17:40:07 -0300

    feat: login route

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, redirect, request, render_template, url_for
 from src.forms.register import RegisterForm
-from src.use_case.user.register_user_use_case import RegisterUserUseCase
-from src.repositories import user_repository
+from src.forms.login import LoginForm
+from src.use_cases.user.register_user_use_case import RegisterUserUseCase
+from src.use_cases.user.login_user_use_case import LoginUserUseCase
 
 
 bp = Blueprint("website", __name__)
@@ -23,7 +24,31 @@ def register():
             result = useCase.execute()
 
             if result == True:
-                redirect(url_for('website.register'))    
+                redirect(url_for("website.register"))
+
             error = useCase.error
 
-    return render_template("register.html", form=form, error=error, title="Taskmaster - Cadastro")
+    return render_template("register.html", title="Taskmaster - Cadastro", form=form, error=error)
+
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    print("entramos na rota")
+
+    if form.validate_on_submit():
+        if request.method == "POST":
+            print("formulário válido")
+            useCase = LoginUserUseCase(form)
+            result = useCase.execute()
+            print("Já executou o caso de uso")
+
+    return render_template("login.html", title="Taskmaster - Login", form=form)
+
+# Nessa rota o usuário já precisa estar autenticado
+
+
+@bp.route("/board")
+def board():
+    return render_template("board.html", title="board")
