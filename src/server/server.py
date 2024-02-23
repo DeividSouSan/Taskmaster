f62928ae0592881f094e6d3979815b241c905393
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-23 06:47:50 -0300

    refactor: server code with application factory

--- src/server/server.py
+++ src/server/server.py
@@ -1,49 +1,42 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from flask_wtf.csrf import CSRFProtect
-
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Carrega as variáveis de ambiente no .eInv
+load_dotenv()
 
-app = Flask(__name__,
-            template_folder="../templates",
-            static_folder="../static")
-
-csrf = CSRFProtect(app)
-
-
-# Capturando as variáveis de ambiente configuradas
 USER = os.environ.get("USER")
 PASSWORD = os.environ.get("PASSWORD")
 DATABASE = os.environ.get("DATABASE")
 
-# Configurando a URI do banco de dados com as credenciais fornecidas
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '12345'
-app.config['WTF_CSRF_SECRET_KEY'] = '12345'
-
-# Se conectando ao banco de dados
-db = SQLAlchemy()
-db.init_app(app)
-engine = create_engine(
-    f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}')
-
-with app.app_context():
-    from src.models.task import Task
-    from src.models.user import User
-
-    # Cria as tabelas
-    db.create_all()
-    print("As tabelas 'user' e 'task' foram criadas com sucesso.")
+DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}'
 
 
-def register_blueprints(app):
-    from src.routes import routes
+def create_app(test_config=None):
+    app = Flask(__name__,
+                template_folder="../templates",
+                static_folder="../static",
+                instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE_URI=DATABASE_URI,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    if app.config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        
+        
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    from ..routes import routes
     app.register_blueprint(routes.bp)
 
-
-register_blueprints(app)
+    return app
