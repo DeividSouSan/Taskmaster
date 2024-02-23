8a4f3d738b0df8a4a1abc60f2d63cafa31a1a0f8
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-23 13:23:54 -0300

    refactor: added login config

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from flask_login import LoginManager
 import os
+
+from ..database.db import Database
+from ..models.user import User
 
 load_dotenv()
 
@@ -21,7 +23,7 @@ def create_app(test_config=None):
 
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE_URI=DATABASE_URI,
+        SQLALCHEMY_DATABASE_URI=DATABASE_URI,
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
@@ -29,14 +31,26 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-        
-        
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
+    from ..database import db
+    db = Database(app)
+    db.initialize_database()
+    db.create_tables()
+
     from ..routes import routes
     app.register_blueprint(routes.bp)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "website.login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     return app
