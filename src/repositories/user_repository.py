c5e8f861152e4a0bce9b62a7173015a380542eca
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-23 06:48:13 -0300

    feat: user repository protocol

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,3 +1,4 @@
+from typing import Protocol
 from flask import current_app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -5,12 +6,20 @@ from sqlalchemy.orm import Session
 from ..models.user import User
 
 
-class UserRepository():
+class IUserRepository(Protocol):
+    def add_user(self, user: User) -> None:
+        ...
+
+    def exists_user_with_field(self, field: str, given_value: str) -> bool:
+        ...
+
+
+class UserRepository:
 
     def __init__(self):
         with current_app.app_context():
             DATABASE_URI = current_app.config['DATABASE_URI']
-            
+
         self.engine = create_engine(DATABASE_URI)
 
     def add_user(self, user):
