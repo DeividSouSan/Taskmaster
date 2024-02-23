765e3f0899e6727f041968967efaa87916419a2b
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-23 06:46:45 -0300

    refactor: improved code readbility and semantics

--- src/use_cases/user/register_user_use_case.py
+++ src/use_cases/user/register_user_use_case.py
@@ -1,51 +1,91 @@
-from src.repositories.user_repository import UserRepository
-from src.models.user import User
-from src.server.server import engine
+"""
+This module implements the Register User use case.
+"""
+
 from datetime import datetime
-import bcrypt
+from ...repositories.user_repository import IUserRepository
+from ...models.user import User
+from ...utils.password_hasher import PassowordHash
+from ...forms.register_form import IRegisterForm
 
 
 class RegisterUserUseCase():
+    """
+    Use case class for registering a new user.
 
-    def __init__(self, form):
-        self.repository = UserRepository(engine)
-        self.form = form
+    This class handles the logic for registering a new user in the system. It
+    checks if the provided username and email are already taken, hashes the password,
+    and creates a new User object with the provided information. The user is then
+    added to the repository.
+
+    Attributes
+        repository (UserRepository): The repository for accessing user data.
+        form (Form): The form containing the user registration data.
+        error (dict): An error object containing information about any validation errors
+        encountered during registration.
+
+    Methods:
+        execute(): Executes the registration use case logic.
+    """
+
+    def __init__(self, form: IRegisterForm, repository: IUserRepository):
+        self.__repository = repository
+        self.__form = form
         self.error = None
 
-    def execute(self):
-        username = self.form.username.data
-        email = self.form.email.data
+    def attempt_registration(self) -> bool:
+        """
+        Executes the registration use case logic.
 
-        invalid_user = self.repository.exists_user_with_username(username)
-        invalid_email = self.repository.exists_user_with_email(email)
+        This method uses the form data to register a new user. It extracts the username and email
+        from the form data and performs the necessary operations to register the user.
 
-        if invalid_user:
-            self.error = {
-                "type": "username",
-                "text": "Já existe um usuário com esse nome."
-            }
+        Returns:
+            False - If the username or email is already taken.
+            True - If the user is successfully registered.
+        """
 
+        username: str = self.__form.username.data
+        email: str = self.__form.email.data
+        password: str = self.__form.password.data
+        fullname: str = self.__form.fullname.data
+        registration_date: datetime = datetime.now()
+
+        if self.is_field_taken("username", username):
+            self.error = {"type": "username", "message": "Username already taken"}
             return False
 
-        if invalid_email:
-            self.error = {
-                "type": "email",
-                "text": "Já existe um usuário com esse email."
-            }
-
+        if self.is_field_taken("email", email):
+            self.error = {"type": "email", "message": "Email already taken"}
             return False
 
-        bytes_pw = bytes(self.form.password.data, "utf-8")
-        password_hash = bcrypt.hashpw(bytes_pw, bcrypt.gensalt())
+        password_hash = PassowordHash().hash_password(password)
 
         user = User(
-            username=self.form.username.data,
+            username=username,
             password_hash=password_hash,
-            fullname=self.form.fullname.data,
-            email=self.form.email.data,
-            registration=datetime.now(),
+            fullname=fullname,
+            email=email,
+            registration=registration_date,
             tasks=[]
         )
 
-        self.repository.add_user(user)
+        self.__repository.add_user(user)
         return True
+
+    def is_field_taken(self, field, value):
+        """
+        Checks if a field is already taken.
+
+        This method checks if a field is already taken by querying the repository for
+        a user with the given field value.
+
+        Args:
+            field (str): The field to check.
+            value (str): The value to check.
+
+        Returns:
+            bool: True if the field is already taken, False otherwise.
+        """
+        field_is_taken = self.__repository.exists_user_with_field(field, value)
+        return field_is_taken
