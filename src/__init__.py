91a6115a357009124b94da635a90c0a9cb23d139
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-24 13:53:02 -0300

    feat: added application factory to src __init__

---
+++ src/__init__.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+USER = os.environ.get("USER")
+PASSWORD = os.environ.get("PASSWORD")
+DATABASE = os.environ.get("DATABASE")
+DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}'
+
+# Initialize Plugins
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def create_app():
+    # Creating App
+    app = Flask(__name__,
+                template_folder="templates",
+                static_folder="static",
+                instance_relative_config=True)
+
+    # Configuring App
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI=DATABASE_URI,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    # Initializing Pluginis
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    with app.app_context():
+        # Create tables
+        from .models.task import Task  # pylint: disable=unused-import
+        from .models.user import User  # pylint: disable=unused-import
+
+        db.create_all()
+
+        # Register Routes
+        from .routes import user_routes, auth_routes, main_routes
+        app.register_blueprint(user_routes.user)
+        app.register_blueprint(auth_routes.auth)
+        app.register_blueprint(main_routes.main)
+
+    return app
