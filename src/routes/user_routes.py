7a85c2afe9d0b20aa2f79d24ca7a2515947ed179
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-25 14:09:35 -0300

    feat:  board "add task modal" feature

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
+from src.forms.task_form import TaskForm
+
 user = Blueprint("user", __name__)
+
+tasks = []
 
 
 @user.route("/board")
 @login_required
 def board():
+    form = TaskForm()
+
     if not current_user.is_authenticated:
         return redirect(url_for("website.login"))
 
-    return render_template("user_specific/board.html", title="board", user=current_user)
+    return render_template(
+        "user_specific/board.html",
+        title="board",
+        user=current_user,
+        tasks=tasks,
+        form=form)
