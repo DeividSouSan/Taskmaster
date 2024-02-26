bc6979aa73d89b6c25dfdcba54305db4205eaae6
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-26 08:55:57 -0300

    feat: first implementation of add task

---
+++ src/use_cases/tasks/add_task_use_case.py
@@ -0,0 +1,23 @@
+from src.models.task import Task
+from src.forms.task_form import TaskForm
+from src.repositories.task_repository import TaskRepository
+
+
+class AddTaskUseCase:
+    def __init__(self, user_id: int, form: TaskForm, repository: TaskRepository):
+        self.__user_id = user_id
+        self.__form = form
+        self.__repository = repository
+
+    def add_task(self):
+        task = self._set_task()
+        self.__repository.add_task(task)
+
+    def _set_task(self):
+        return Task(
+            user_id=self.__user_id,
+            task_title=self.__form.task_title.data,
+            task_description=self.__form.task_description.data,
+            due_date=self.__form.due_date.data,
+            status=int(self.__form.status.data)
+        )
