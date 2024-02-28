5cb760d910ba7aa3ed967b34aaaab2d8bb4250f4
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-28 07:03:24 -0300

    feat + ref: added flash messages and user instance creation isolated from attempt_registration

--- src/use_cases/user/register_user_use_case.py
+++ src/use_cases/user/register_user_use_case.py
@@ -1,8 +1,6 @@
-"""
-This module implements the Register User use case.
-"""
-
 from datetime import datetime
+from flask import flash
+
 from ...repositories.user_repository import IUserRepository
 from ...models.user import User
 from ...utils.password_hasher import PassowordHash
@@ -16,29 +14,16 @@ class RegisterUserUseCase():
         self.error = None
 
     def attempt_registration(self) -> bool:
-        username: str = self.__form.username.data
-        email: str = self.__form.email.data
-        password: str = self.__form.password.data
-        fullname: str = self.__form.fullname.data
-        registration_date: datetime = datetime.now()
 
-        if self.is_field_taken("username", username):
-            self.error = {"type": "username", "message": "Username already taken"}
+        user = self.create_user()
+
+        if self.is_field_taken("username", user.username):
+            flash("Nome de usuário já foi utilizado")
             return False
 
-        if self.is_field_taken("email", email):
-            self.error = {"type": "email", "message": "Email already taken"}
+        if self.is_field_taken("email", user.email):
+            flash("Email já foi utlizado")
             return False
-
-        password_hash = PassowordHash().hash_password(password)
-
-        user = User(
-            username=username,
-            password_hash=password_hash,
-            fullname=fullname,
-            email=email,
-            registration=registration_date,
-        )
 
         self.__repository.add_user(user)
         return True
@@ -46,3 +31,12 @@ class RegisterUserUseCase():
     def is_field_taken(self, field, value):
         field_is_taken = self.__repository.exists_user_with_field(field, value)
         return field_is_taken
+
+    def create_user(self):
+        return User(
+            username=self.__form.username.data,
+            password_hash=PassowordHash().hash_password(self.__form.password.data),
+            fullname=self.__form.fullname.data,
+            email=self.__form.email.data,
+            registration=datetime.now(),
+        )
