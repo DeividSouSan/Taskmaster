fcde1debba1bdf37628a0df400f41eaf8affff9c
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-02-29 09:14:34 -0300

    fix: due_date error when null fixed

--- src/models/task.py
+++ src/models/task.py
@@ -26,5 +26,5 @@ class Task(db.Model):
     user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"))
     task_title: Mapped[str] = mapped_column(String(50))
     task_description: Mapped[str] = mapped_column(String(200))
-    due_date: Mapped[datetime]
+    due_date: Mapped[datetime] = mapped_column(nullable=True)
     status: Mapped[int]
