52f1c405dc526005fd38667489c6bf017ffcc5db
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-03-14 06:37:57 -0300

    refactor: type hint and docstring

--- src/utils/password_hasher.py
+++ src/utils/password_hasher.py
@@ -1,17 +1,21 @@
 import bcrypt
 
 
-class PassowordHash():
+class PasswordHash():
 
     @staticmethod
-    def hash_password(password):
+    def hash_password(password: str) -> bytes:
         bytes_pw = bytes(password, "utf-8")
         password_hash = bcrypt.hashpw(bytes_pw, bcrypt.gensalt())
         return password_hash
 
     @staticmethod
-    def check_password(password, hashed_password):
+    def check_password(password: str, hashed_password: str) -> bool:
+        """
+        the hashed_password comes from the database as a string
+        """
+
         password = bytes(password, "utf-8")
         hashed_password = bytes(hashed_password, "utf-8")
-        
+
         return bcrypt.checkpw(password, hashed_password)
