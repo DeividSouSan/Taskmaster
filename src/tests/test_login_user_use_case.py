9106b25d8736b8001450c9b313bf8c611b0456e8
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-03-14 06:35:20 -0300

    feat: some login use case tests

---
+++ src/tests/test_login_user_use_case.py
@@ -0,0 +1,59 @@
+import unittest
+from unittest.mock import Mock, patch
+from src import create_app
+from flask import current_app
+from src.use_cases.user.login_user_use_case import LoginUserUseCase
+
+
+class TestLoginUserUseCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.form_mock = Mock()
+        self.repository_mock = Mock()
+        self.pwdHasher_mock = Mock()
+        self.usecase = LoginUserUseCase(
+            self.form_mock, self.repository_mock, self.pwdHasher_mock)
+
+    def test_attempt_login_user_with_valid_credentials(self):
+        
+        # Dados do formulário enviado
+        self.form_mock.username.data = "username"
+        self.form_mock.password.data = "password"
+        
+        # Existe usuário no banco de dados
+        self.repository_mock.exists_user_with_field.return_value = True
+        self.repository_mock.get_user_password_by_username.return_value = "hashed_password"
+        self.pwdHasher_mock.check_password.return_value = True
+
+        # Simula a aplicação e um contexto de requisição
+        with self.app.app_context():
+            with self.app.test_request_context():
+                result = self.usecase.attempt_login_user()
+
+        self.assertTrue(result)
+        self.repository_mock.get_user_by_username.assert_called_once_with(
+            "username")
+        self.repository_mock.exists_user_with_field.assert_called_once_with(
+            "username", "username")
+        self.repository_mock.get_user_password_by_username.assert_called_once_with(
+            "username")
+        self.pwdHasher_mock.check_password.assert_called_once_with(
+            "password", "hashed_password")
+        
+    def test_attempt_login_user_not_registered_(self):
+        
+        # Dados do formulário enviado
+        self.form_mock.username.data = "username"
+        self.form_mock.password.data = "password"
+        
+        # Não existe o  usuário no banco de dados
+        self.repository_mock.exists_user_with_field.return_value = False
+
+        # Simula a aplicação e um contexto de requisição
+        with self.app.app_context():
+            with self.app.test_request_context():
+                result = self.usecase.attempt_login_user()
+
+        self.assertFalse(result)
+        self.repository_mock.exists_user_with_field.assert_called_once_with("username", "username")
