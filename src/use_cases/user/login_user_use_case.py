63d48ed45a3c156829578ca9525a4589e2240443
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-03-14 06:37:09 -0300

    feat: use case pwd_hasher attribute

--- src/use_cases/user/login_user_use_case.py
+++ src/use_cases/user/login_user_use_case.py
@@ -1,16 +1,17 @@
 from flask import flash
 from flask_login import login_user
 
-from ...utils.password_hasher import PassowordHash
+from ...utils.password_hasher import PasswordHash
 from ...forms.login_form import LoginForm
 from ...repositories.user_repository import UserRepository
 
 
 class LoginUserUseCase():
 
-    def __init__(self, form: LoginForm, repository: UserRepository):
+    def __init__(self, form: LoginForm, repository: UserRepository, pwd_hasher: PasswordHash):
         self.__repository = repository
         self.__form = form
+        self.__pwd_hasher = pwd_hasher
 
     def attempt_login_user(self):
         username = self.__form.username.data
@@ -21,21 +22,18 @@ class LoginUserUseCase():
         if valid_credentials:
             user = self.__repository.get_user_by_username(username)
             login_user(user)
-
             return True
 
         return False
 
     def verify_credentials(self, username, password):
-
         user_exists = self.__repository.exists_user_with_field(
             "username", username)
-
         if user_exists:
             database_password = self.__repository\
                 .get_user_password_by_username(username)
 
-            pwd_is_correct = PassowordHash()\
+            pwd_is_correct = self.__pwd_hasher\
                 .check_password(password, database_password)
 
             if not pwd_is_correct:
@@ -45,3 +43,5 @@ class LoginUserUseCase():
 
         flash("Usuário não encontrado", "error")
         return False
+
+   
