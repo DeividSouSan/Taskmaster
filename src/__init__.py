ba1e871e265afd110a533ea9232b97efe8e3b6dd
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-03-18 15:15:24 -0300

    feat: session lifetime of 30 minutes

--- src/__init__.py
+++ src/__init__.py
@@ -4,17 +4,21 @@ from dotenv import load_dotenv
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_htmx import HTMX
+from datetime import timedelta
 
 load_dotenv()
+
 USER = os.environ.get("USER")
 PASSWORD = os.environ.get("PASSWORD")
 DATABASE = os.environ.get("DATABASE")
+
 DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@localhost/{DATABASE}'
 
 # Initialize Plugins
 db = SQLAlchemy()
 login_manager = LoginManager()
 htmx = HTMX()
+# CSRF_TOKEN
 
 
 def create_app():
@@ -28,10 +32,11 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI=DATABASE_URI,
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        PERMANENT_SESSION_LIFETIME=timedelta(minutes=30)
     )
 
-    # Initializing Pluginis
+    # Initializing Plugins
     db.init_app(app)
     login_manager.init_app(app)
     htmx.init_app(app)
