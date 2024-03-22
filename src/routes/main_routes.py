eae551847b055792bb945c9f9592e737b923dfbe
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-03-22 08:19:17 -0300

    removed: main routes

--- src/routes/main_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-main = Blueprint("main", __name__)
-
-
-@main.route("/")
-def index():
-    return render_template("index.html", title="Home - Taskmaster")
