f4706c6fa9bac57bda40f78c45863bdd31416049
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-06 18:03:22 -0300

    refactor: html and js files

--- src/static/javascript/board.js
+++ src/static/javascript/board.js
@@ -1,40 +1,15 @@
 
 /* Botão de adicionar tarefa */
 
-var addTaskButton = document.querySelector(".add-task-button");
 
-const taskFormTemplate = document.querySelector(".add-task-template");
-const taskFormModal = taskFormTemplate.content.cloneNode(true);
-const taskFormContainer = taskFormModal.querySelector(".modal-container");
 
-addTaskButton.addEventListener('click', () => {
-    document.body.appendChild(taskFormContainer);
-})
-
-taskFormContainer.addEventListener("click", (ev) => {
-    if (ev.target.classList.contains("modal-container")) {
-        document.body.removeChild(taskFormContainer);
-    }
-})
 
 /* Botão de opções do menu */
 
-const accountButton = document.querySelector(".account-button");
-const floatingMenu = document.querySelector(".floating-menu-container");
-const headerAfterAuth = document.querySelector(".header-after-auth");
-
-accountButton.addEventListener('click', () => {
-    if (floatingMenu.classList.contains("active")) {
-        floatingMenu.style.display = "none";
-    } else {
-        floatingMenu.style.display = "block";
-    }
-    floatingMenu.classList.toggle("active");
-});
 
 /* Confimração de Excluir a Task (Task Delegation Technique)*/
 
-/* 
+/*
 Quando você adiciona um evento de clique ao documento, esse evento é acionado sempre que um clique é feito em qualquer lugar do documento. Isso é chamado de propagação de eventos, onde um evento começa no elemento mais interno (o alvo do evento) e então se propaga para cima na árvore do DOM.
 
 Se você atribuir o evento de clique diretamente ao botão, o evento será perdido sempre que o botão for re-renderizado pelo HTMX, porque o HTMX substitui o elemento do botão no DOM, e o novo botão não terá o evento de clique anexado a ele.
@@ -58,5 +33,3 @@ taskWrapper.addEventListener('click', event => {
         });
     }
 });
-
-
