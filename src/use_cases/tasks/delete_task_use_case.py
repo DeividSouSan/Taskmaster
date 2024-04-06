15a4228784e1a8f205dbe1eaf1e5cd473f1d4c41
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-06 19:05:24 -0300

    refactor: only "execute" method added that acess repository

--- src/use_cases/tasks/delete_task_use_case.py
+++ src/use_cases/tasks/delete_task_use_case.py
@@ -1,10 +1,10 @@
 from src.repositories.task_repository import TaskRepository
 
 
-class MoveTaskToTrashUseCase:
-    def __init__(self, task_id, repository: TaskRepository):
+class DeleteTaskUseCase:
+    def __init__(self, task_id: int, repository: TaskRepository):
         self.__task_id = task_id
         self.__repository = repository
 
-    def move_task_to_trash(self):
-        return self.__repository.move_to_trash(self.__task_id)
+    def execute(self):
+        return self.__repository.delete_task(self.__task_id)
