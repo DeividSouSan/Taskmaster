42cba8bcd399ac916b2a8f7950e5058e01d89ebd
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-06 18:38:03 -0300

    feat: check_field_whitespaces util

---
+++ src/utils/check_field_whitespaces.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+
+class CheckFieldWhitespaces:
+    @staticmethod
+    def is_field_with_whitespaces(form: FlaskForm):
+        for field in form:
+            if field.name  not in ["csrf_token", "submit"]:
+                if field.data.startswith(" ") or field.data.endswith(" "):
+                    return True
+        return False
