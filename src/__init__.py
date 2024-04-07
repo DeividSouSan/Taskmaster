024368071c863aee0f35e1be93607d5fca14b580
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-07 18:46:27 -0300

    refactor: add create_app parameter "config"

--- src/__init__.py
+++ src/__init__.py
@@ -13,12 +13,12 @@ htmx = HTMX()
 csrf = CSRFProtect()
 
 
-def create_app():
+def create_app(config: str):
     # Creating App
     app = Flask(__name__)
 
     # Configuring App
-    app.config.from_object("src.config.DevelopmentConfig")
+    app.config.from_object(f"src.config.{config}")
 
     # Initializing Plugins
     db.init_app(app)
@@ -34,11 +34,10 @@ def create_app():
         db.create_all()
 
         # Register Routes
-        from .routes import auth_routes, form_routes, task_routes, view_routes
+        from .routes import auth_routes, task_routes, view_routes
 
         app.register_blueprint(task_routes.task)
         app.register_blueprint(auth_routes.auth)
         app.register_blueprint(view_routes.view)
-        app.register_blueprint(form_routes.form)
 
     return app
