6787def27578fdb7cb75a839c6ddb254b227b1b4
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-08 06:09:32 -0300

    feat: injected CheckFormFields and UserLoginNotifier classes

--- src/use_cases/user/login_user_use_case.py
+++ src/use_cases/user/login_user_use_case.py
@@ -1,6 +1,8 @@
 from flask import flash
 from flask_login import login_user
 
+from src.utils.user_login_notifier import UserLoginNotifier
+
 from ...forms.login_form import LoginForm
 from ...repositories.user_repository import UserRepository
 from ...utils.check_form_fields import CheckFormFields
@@ -10,21 +12,25 @@ from ...utils.password_hasher import PasswordHash
 class LoginUserUseCase:
 
     def __init__(
-        self, form: LoginForm, repository: UserRepository, pwd_hasher: PasswordHash
+        self, form: LoginForm, repository: UserRepository, pwd_hasher: PasswordHash, check_form_fields: CheckFormFields, notifier: UserLoginNotifier
     ):
         self.__repository = repository
         self.__form = form
         self.__pwd_hasher = pwd_hasher
+        self.__check_form_fields = check_form_fields
+        self.__notifier = notifier
 
     def attempt_login_user(self) -> bool:
 
-        if CheckFormFields.is_field_with_whitespaces(self.__form):
-            self.notify_field_with_whitespaces()
+        # Checa se os dados possuem espaços em branco
+        if self.__check_form_fields.is_field_with_whitespaces(self.__form):
+            self.__notifier.notify_field_with_whitespaces()
             return False
 
         username = self.__form.username.data
         password = self.__form.password.data
 
+        # Checa se o usuário existe e se a senha está correta
         valid_credentials = self.verify_credentials(username, password)
 
         if valid_credentials:
@@ -50,18 +56,11 @@ class LoginUserUseCase:
             )
 
             if not pwd_is_correct:
-                self.notify_wrong_password()
+                self.__notifier.notify_wrong_password()
 
             return pwd_is_correct
 
-        self.notify_user_not_found()
+        self.__notifier.notify_user_not_found()
         return False
 
-    def notify_user_not_found(self) -> None:
-        flash("Usuário não encontrado", "error")
-
-    def notify_wrong_password(self) -> None:
-        flash("Senha incorreta", "error")
-
-    def notify_field_with_whitespaces(self) -> None:
-        flash("Os campos não podem começar ou terminar com espaços", "error")
+    
