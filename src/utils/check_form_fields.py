046ef6309466571699ae0d8d0777e0de33ede037
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-08 06:11:25 -0300

    feat: added two classes, one check whitespaces, the other uniqueness of field

--- src/utils/check_form_fields.py
+++ src/utils/check_form_fields.py
@@ -1,22 +1,20 @@
 from flask_wtf import FlaskForm
-
 from ..repositories.user_repository import UserRepository
 
-
-class CheckFormFields:
-    @staticmethod
-    def is_field_with_whitespaces(form: FlaskForm):
+class FieldWhitespaceChecker:
+    def is_field_with_whitespaces(self, form: FlaskForm):
         for field in form:
             if field.name not in ["csrf_token", "submit"]:
                 if field.data.startswith(" ") or field.data.endswith(" "):
                     return True
         return False
 
+class FieldUniquenessChecker:
+    def __init__(self, repository: UserRepository):
+        self.__repository = repository
 
-    @staticmethod
-    def is_field_taken(self, values: list[dict[str, str]], repository: UserRepository) -> str | None:
+    def is_field_taken(self, values: list[dict[str, str]]) -> str | None:
         for field in values:
-            if repository.exists_user_with_field(field["name"], field["value"]):
+            if self.__repository.exists_user_with_field(field["name"], field["value"]):
                 return field["name"]
-
         return None
