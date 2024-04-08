fea47173062783bcc53e5fc18b46c7a9a62eb06b
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-08 06:23:20 -0300

    chore: just black formated

--- src/utils/check_form_fields.py
+++ src/utils/check_form_fields.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
+
 from ..repositories.user_repository import UserRepository
+
 
 class FieldWhitespaceChecker:
     def is_field_with_whitespaces(self, form: FlaskForm):
@@ -8,6 +10,7 @@ class FieldWhitespaceChecker:
                 if field.data.startswith(" ") or field.data.endswith(" "):
                     return True
         return False
+
 
 class FieldUniquenessChecker:
     def __init__(self, repository: UserRepository):
