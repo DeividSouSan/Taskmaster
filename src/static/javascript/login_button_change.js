a699dbb78c5d65ca3d52b05aae495f9ee2f3f882
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-12 06:32:18 -0300

    feat: submit button disable logic

--- src/static/javascript/login_button_change.js
+++ src/static/javascript/login_button_change.js
@@ -1,27 +1,27 @@
 const loginForm = document.querySelector(".login-form");
+
 const loginUsernameInput = loginForm.querySelector("#username");
 const loginPasswordInput = loginForm.querySelector("#password");
+
 const loginSubmitButton = loginForm.querySelector("#submit");
 
-loginUsernameInput.addEventListener('keyup', event => {
-    const allFieldsFilled = loginUsernameInput.value.length > 0 && loginPasswordInput.value.length > 0;
+const checkIsFilled = (input) => {
+    if (input.value.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const fields = [loginUsernameInput, loginPasswordInput];
+
+loginForm.addEventListener('keyup', event => {
+    const allFieldsFilled = fields.every(checkIsFilled);
 
     if (allFieldsFilled) {
         loginSubmitButton.disabled = false;
-
     } else {
         loginSubmitButton.disabled = true;
-
     }
 });
 
-loginPasswordInput.addEventListener('keyup', event => {
-    const allFieldsFilled = loginUsernameInput.value.length > 0 && loginPasswordInput.value.length > 0
-
-    if (allFieldsFilled) {
-        loginSubmitButton.disabled = false;
-    } else {
-        loginSubmitButton.disabled = true;
-
-    }
-});
