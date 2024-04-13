dd60182ae6645765a8f3185861195bbd430cae2a
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-13 09:10:40 -0300

    chore: deleted some comments

--- src/static/javascript/board.js
+++ src/static/javascript/board.js
@@ -1,15 +1,5 @@
+/* Confimração de Excluir a Task (Task Delegation Technique)
 
-/* Botão de adicionar tarefa */
-
-
-
-
-/* Botão de opções do menu */
-
-
-/* Confimração de Excluir a Task (Task Delegation Technique)*/
-
-/*
 Quando você adiciona um evento de clique ao documento, esse evento é acionado sempre que um clique é feito em qualquer lugar do documento. Isso é chamado de propagação de eventos, onde um evento começa no elemento mais interno (o alvo do evento) e então se propaga para cima na árvore do DOM.
 
 Se você atribuir o evento de clique diretamente ao botão, o evento será perdido sempre que o botão for re-renderizado pelo HTMX, porque o HTMX substitui o elemento do botão no DOM, e o novo botão não terá o evento de clique anexado a ele.
