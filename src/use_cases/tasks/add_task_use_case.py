304185ae94f0f30ccfbf4c99e0cdcf9a782f7ef1
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-15 06:01:09 -0300

    refactor: attributes visibility is now protected

--- src/use_cases/tasks/add_task_use_case.py
+++ src/use_cases/tasks/add_task_use_case.py
@@ -5,17 +5,17 @@ from src.repositories.task_repository import TaskRepository
 
 class AddTaskUseCase:
     def __init__(self, user_id: int, form: TaskForm, repository: TaskRepository):
-        self.__user_id = user_id
-        self.__form = form
-        self.__repository = repository
+        self._user_id = user_id
+        self._form = form
+        self._repository = repository
 
     def execute(self):
-        task = self._create_object(self.__form)
-        self.__repository.add_task(task)
+        task = self._create_object(self._form)
+        self._repository.add_task(task)
 
     def _create_object(self, form):
         return Task(
-            user_id=self.__user_id,
+            user_id=self._user_id,
             title=form.title.data,
             description=form.description.data,
             due_date=form.due_date.data,
