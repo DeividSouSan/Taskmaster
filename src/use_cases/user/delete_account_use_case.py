affb04050691c7a656251edc69ef82d6cf8b27c5
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-15 07:01:51 -0300

    refactor: delete account use case

--- src/use_cases/user/delete_account_use_case.py
+++ src/use_cases/user/delete_account_use_case.py
@@ -4,16 +4,28 @@ from src.repositories.user_repository import UserRepository
 
 
 class DeleteAccountUseCase:
+    """
+    Delete user account use case.
 
-    def __init__(self, repository: UserRepository):
+    This use case is responsible for deleting a user account from the database.
+
+    Attributes:
+        user_id: The id of the user to be deleted.
+        repository: The repository that handles the database operations for the User model.
+
+    Returns:
+        None.
+
+    """
+
+    def __init__(self, user_id: int, repository: UserRepository):
+        self._user_id = user_id
         self._repository = UserRepository()
 
-    def delete_account(self, user_id):
-        success = self._repository.delete_user_by_id(user_id)
-
-        if success:
+    def execute(self) -> None:  # pylint: disable=missing-function-docstring
+        try:
+            self._repository.delete_user_by_id(self._user_id)
             flash("Conta deletada com sucesso", "error")
-            return True
-        else:
+        except Exception as e:
+            print(e)
             flash("Delete todas as tarefas antes de deletar a conta", "error")
-            return False
