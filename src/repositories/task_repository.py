806579adbab8dfd6c55646490925adfe0ede5ab2
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-15 06:03:13 -0300

    refactor: added docstring

--- src/repositories/task_repository.py
+++ src/repositories/task_repository.py
@@ -6,47 +6,140 @@ from src.models.task import Task
 
 
 class TaskRepository:
+    """
+    Repository Class for the Task model.
+
+    Responsible for handling the database operations for the Task model. It has methods for adding, getting, updating and deleting tasks from the database.
+
+    The SQLALCHEMY_DATABASE_URI is obtained from the current_app.config, which is the configuration of the Flask application. This way, the repository can be used in the application context.
+
+    If you want to change the database, you can change the SQLACLHEMY_DATABASE_URI in the configuration file of the application config.py.
+
+    Attributes:
+        _engine: The SQLAlchemy engine that connects to the database.
+    """
+
     def __init__(self):
         with current_app.app_context():
             DATABASE_URI = current_app.config["SQLALCHEMY_DATABASE_URI"]
 
-        self.__engine = create_engine(DATABASE_URI)
+        self._engine = create_engine(DATABASE_URI)
 
-    def add_task(self, task: Task):
-        with Session(self.__engine) as session:
-            session.add(task)
-            session.commit()
+    def add_task(self, task: Task) -> bool:
+        """
+        Adds a task to the database.
+
+        Througn the session context manager, it adds the task to the database and commits the transaction.
+
+        Args:
+            task: The Task object to be added to the database.
+
+        Returns:
+            bool: True if the task was added successfully, False otherwise.
+        """
+        with Session(self._engine) as session:
+            # TODO: improve the exception handling, and return.
+
+            try:
+                session.add(task)
+                session.commit()
+
+                return True
+            except:
+                return False
 
     def get_tasks(self, user_id: int):
-        with Session(self.__engine) as session:
+        """
+        Get all tasks from the database.
+
+        Through the session context manager, it queries the database for all tasks of a specific user.
+
+        Args:
+            user_id: The id of the user that owns the tasks.
+
+        Returns:
+            Query[Task]: A list with all tasks of the user.
+        """
+        with Session(self._engine) as session:
             tasks = session.query(Task).filter(Task.user_id == user_id)
             return tasks
 
     def get_task_by_id(self, task_id: int):
-        with Session(self.__engine) as session:
+        """
+        Query the database for a task by its id.
+
+        Through the session context manager, it queries the database for a task with a specific id.
+
+        Args:
+            task_id: The id of the task to be queried.
+
+        Returns:
+            Task: The task with the id task_id.
+        """
+        with Session(self._engine) as session:
             task = session.query(Task).filter(Task.id == task_id).first()
             return task
 
     def get_tasks_like(self, user_id: int, text: str):
-        with Session(self.__engine) as session:
-            print(text)
+        """
+        Query the database for tasks that have a specific text in the title.
+
+        Through the session context manager, it queries the database for tasks of a specific user that have a specific text in the title.
+
+        Args:
+            user_id: The id of the user that owns the tasks.
+            text: The text that the title of the tasks must contain.
+
+        Returns:
+            Query[Task]: A list with all tasks of the user that have the text in the title.
+        """
+        with Session(self._engine) as session:
             tasks = session.query(Task).filter(
                 Task.user_id == user_id, Task.title.like(f"%{text}%")
             )
             return tasks
 
-    def update_task(self, task_id: int, values: dict[str, any]):
+    def update_task(self, task_id: int, values: dict[str, any]) -> bool:
+        """
+        Update a task in the database.
 
-        with Session(self.__engine) as session:
-            row_matched = session.query(Task).filter(Task.id == task_id).update(values)
-            print(row_matched)
-            session.commit()
+        Through the session context manager, it updates a task in the database with the values in the dictionary.
 
-    def delete_task(self, task_id: int):
-        with Session(self.__engine) as session:
+        Args:
+            task_id: The id of the task to be updated.
+            values: A dictionary with the values to be updated.
+
+        Returns:
+            bool: True if the task was updated successfully, False otherwise.
+        """
+        with Session(self._engine) as session:
+            # TODO: improve the exception handling, and return.
+            try:
+                session.query(Task).filter(Task.id == task_id).update(values)
+                session.commit()
+
+                return True
+            except:
+                return False
+
+    def delete_task(self, task_id: int) -> bool:
+        """
+        Delete a task from the database.
+
+        Through the session context manager, it deletes a task from the database.
+
+        Args:
+            task_id: The id of the task to be deleted.
+
+        Returns:
+            bool: True if the task was deleted successfully, False otherwise.
+        """
+        with Session(self._engine) as session:
             task = session.query(Task).filter(Task.id == task_id).first()
 
             if task is not None:
                 session.delete(task)
+                session.commit()
+                return True
 
-            session.commit()
+            return False
--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -6,15 +6,39 @@ from ..models.user import User
 
 
 class UserRepository:
+    """
+    Repository Class for the User model.
+
+    Responsible for handling the database operations for the User model. It has methods for adding, getting, updating and deleting users from the database.
+
+     The SQLALCHEMY_DATABASE_URI is obtained from the current_app.config, which is the configuration of the Flask application. This way, the repository can be used in the application context.
+
+    If you want to change the database, you can change the SQLACLHEMY_DATABASE_URI in the configuration file of the application config.py.
+
+    Attributes:
+        _engine: The SQLAlchemy engine that connects to the database.
+    """
 
     def __init__(self):
         with current_app.app_context():
             DATABASE_URI = current_app.config["SQLALCHEMY_DATABASE_URI"]
 
-        self.engine = create_engine(DATABASE_URI)
+        self._engine = create_engine(DATABASE_URI)
 
     def add_user(self, user: User):
-        with Session(self.engine) as session:
+        """
+        Adds a user to the database.
+
+        Througn the session context manager, it adds the user to the database and commits the transaction.
+
+        Args:
+            user: The User object to be added to the database.
+
+        Returns:
+            bool: True if the user was added successfully, False otherwise.
+        """
+        with Session(self._engine) as session:
+            # TODO: improve the exception handling, and return.
             try:
                 session.add(user)
                 session.commit()
@@ -23,23 +47,24 @@ class UserRepository:
                 return False
 
     def get_user_password_by_username(self, username) -> str:
-        with Session(self.engine) as session:
+
+        with Session(self._engine) as session:
             user = session.query(User).filter(User.username == username).first()
             return user.password_hash
 
     def get_user_by_username(self, username) -> User:
-        with Session(self.engine) as session:
+        with Session(self._engine) as session:
             user = session.query(User).filter(User.username == username).first()
             return user
 
     def exists_user_with_field(self, field, given_value) -> bool:
-        with Session(self.engine) as session:
+        with Session(self._engine) as session:
             user_attribute = getattr(User, field)
             result = session.query(User).filter(user_attribute == given_value).first()
             return bool(result)
 
     def delete_user_by_id(self, user_id) -> bool:
-        with Session(self.engine) as session:
+        with Session(self._engine) as session:
             user = session.query(User).filter(User.id == user_id).first()
 
             try:
@@ -52,7 +77,7 @@ class UserRepository:
                 return False
 
     def delete_table(self):
-        with Session(self.engine) as session:
+        with Session(self._engine) as session:
             session.query(User).delete()
             session.commit()
             return True
