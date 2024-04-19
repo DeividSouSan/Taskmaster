b800a4075cecb7ce6fdba103511ec3ce301a75fd
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-19 06:29:07 -0300

    feat: config is now a environment variable

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from src import create_app
 
-app = create_app("DevelopmentConfig")
+load_dotenv()
+config = os.environ.get("CONFIG")
+
+app = create_app(config)
 
 if __name__ == "__main__":
     app.run(port=3000, host="0.0.0.0", debug=True)
