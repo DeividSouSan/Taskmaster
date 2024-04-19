fec92b7f7a550db0e36f32498df3705aabc6b0dc
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-04-19 08:07:15 -0300

    feat: database uri now is absolute

--- api/src/config.py
+++ api/src/config.py
@@ -9,33 +9,32 @@ load_dotenv()
 class Config:
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     SECRET_KEY = os.environ.get("SECRET_KEY_DEV")
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI_SQLITE")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
+    db_path = os.path.join(os.path.dirname(__file__), 'taskmaster.db')
+    db_uri = f'sqlite+pysqlite:///{db_path}'
+    SQLALCHEMY_DATABASE_URI = db_uri
+    
     DEBUG = True
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
 
 
 class TestingConfig(Config):
     SECRET_KEY = os.environ.get("SECRET_KEY_TEST")
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI_SQLITE_TEST")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     DEBUG = True
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
 
 
 class ProductionConfig(Config):
-    SECRET_KEY = os.environ.get("SECRET_KEY_PRODUCTION")
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI_SQLITE")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    DEBUG = False
+    SECRET_KEY = os.environ.get("SECRET_KEY_PROD")
+    
+    db_path = os.path.join(os.path.dirname(__file__), 'taskmaster.db')
+    db_uri = f'sqlite+pysqlite:///{db_path}'
+    SQLALCHEMY_DATABASE_URI = db_uri
+    
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
